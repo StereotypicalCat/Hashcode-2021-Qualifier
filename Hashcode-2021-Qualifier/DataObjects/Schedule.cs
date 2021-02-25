9ccc058d1bb335f54a4379257fe94506d28e0668
lassees <lasse.g.eskildsen@gmail.com>
2021-02-25 19:10:05 +0100

    Added schedule as a outputter for oute solution

---
+++ Hashcode-2021-Qualifier/DataObjects/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashcode_2021_Qualifier.DataObjects
+{
+    public class Schedule
+    {
+        private List<Intersection> intersections = new List<Intersection>();
+
+
+    }
+}
